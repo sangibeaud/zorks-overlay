37936f54af431412abeb33148988fc3669aa87db
sangibeaud <stephane.angibeaud@gmail.com>
2023-03-19 18:18:05 +0100

    Update emerge all shell

--- tests/emerge-all-ebuilds.sh
+++ tests/emerge-all-ebuilds.sh
@@ -9,7 +9,7 @@ fi
 SCRIPT_PATH=$(dirname "$0")
 
 #EBUILDS=($(git ls-files | grep -e "\.ebuild$" | egrep -v '.+9999(-r[0-9]+)?.ebuild'))
-EBUILDS=$(grep -R -l "^KEYWORDS=\".*[~ ]amd64" `find . -name *.ebuild` | egrep -v '.+9999(-r[0-9]+)?.ebuild' | grep -vf tests/resources/blacklist.txt sed -e "s/^\.\///")
+EBUILDS=$(grep -R -l "^KEYWORDS=\".*[~ ]amd64" `find . -name *.ebuild` | egrep -v '.+9999(-r[0-9]+)?.ebuild' | grep -vf tests/resources/blacklist.txt | sed -e "s/^\.\///")
 echo "Emerging the following ebuilds: ${EBUILDS[@]}"
 
 # Emerge the ebuilds in a clean stage3
